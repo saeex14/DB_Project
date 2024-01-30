bc19f919499acdb4f7f209934391a63e7d8a7497
AmirrezaGholizadeh <amirrezabyhimself@gmail.com>
2024-01-30 10:51:13 +0330

    Creating Account_Info table

--- DataDefinitionLanguage.sql
+++ DataDefinitionLanguage.sql
@@ -7,4 +7,12 @@ email VARCHAR(50),
 phone_number VARCHAR(11),
 salt UNIQUEIDENTIFIER,
 PRIMARY KEY(username)
-)
+);
+
+create table Account_Info(
+account_number VARCHAR(16),
+username VARCHAR(25),
+amount DECIMAL(15,2),
+block BIT,
+loan_status BIT
+);
